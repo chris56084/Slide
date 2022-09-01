5530a0a99922984ac853fd38f38484169f5e258f
chris reed <reed.chris@gmail.com>
2022-08-31 18:15:02 -0700

    new slide program

---
+++ slide.py
@@ -0,0 +1,26 @@
+from math import degrees, sqrt, atan
+
+a = input("Enter the rise of the slide's ladder: ")
+b = input("Enter the run of the slide's ladder: ")
+
+rise = int(a)
+run = int(b)
+
+sidea = rise * rise
+sideb = run * run
+slide = int(sqrt(sidea + sideb))
+angle = int(degrees(atan(rise/run)))
+
+slidesafe = False
+
+if angle > 40:
+    slidesafe = False
+elif rise > 6:
+    slidesafe = False
+else:
+    slidesafe = True
+
+if slidesafe == True:
+    print("Safe! The angle is: " + str(angle) + " degrees and your slide length is " + str(slide))
+else:
+    print("Safe")
